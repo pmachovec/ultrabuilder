e5faac6658ddf5490df42faf3f993e5634b71054
Petr Machovec <pmachovec@outlook.com>
2019-12-11 18:44:32 +0100

    New Githooker version used

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
     `java-gradle-plugin`
     `maven-publish`
     kotlin("jvm").version("1.3.50")
-    id("com.pmachovec.githooker").version("1.0")
-    id("org.jlleitschuh.gradle.ktlint") version "9.0.0"
+    id("com.pmachovec.githooker").version("1.0.1")
+    id("org.jlleitschuh.gradle.ktlint").version("9.0.0")
 }
 
 group = "com.pmachovec"
-version = "1.0"
+version = "1.0.1"
 
 // REPOSITORIES AND DEPENDENCIES
 repositories {
@@ -26,7 +26,7 @@ dependencies {
     testImplementation("org.powermock", "powermock-module-testng", "2.0.2")
     testImplementation("org.testng", "testng", "7.0.0")
 
-    runtime(files(sourceSets["main"].output.resourcesDir))
+    runtimeOnly(files(sourceSets["main"].output.resourcesDir))
 }
 
 // PUBLICATION TO MAVEN REPOSITORY
